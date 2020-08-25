4e2a9ab4a3e4aca07743a18d8bc3687b29d51dfc
CCease <Vanish001@Live.com>
2020-08-25 09:53:53 +0800

    Update AcccountDao. Implement a few functions.

--- src/main/java/com/dcoms/dao/impl/AccountDao.java
+++ src/main/java/com/dcoms/dao/impl/AccountDao.java
@@ -13,11 +13,49 @@ public class AccountDao implements IAccountDao {
 
     @Override
     public List<Account> findAllAccount() {
+
+        List<Account> accountList = null;
+        PreparedStatement st;
+        ResultSet rs;
+        String query = "SELECT * FROM `users`";
+        try{
+            st = My_CNX.getConnection().prepareStatement(query);
+            rs = st.executeQuery();
+
+            while (rs.next()){
+                accountList.add(accountDataHandler(rs));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        if (accountList == null){
+            return accountList;
+        }
         return null;
+
     }
 
     @Override
     public Account findAccountById(String id) {
+
+        PreparedStatement st;
+        ResultSet rs;
+        String query = "SELECT * FROM `users` WHERE `id` = ?";
+        try{
+            st = My_CNX.getConnection().prepareStatement(query);
+            st.setString(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+                return accountDataHandler(rs);
+            }else{
+                return null;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -33,16 +71,7 @@ public class AccountDao implements IAccountDao {
             rs = st.executeQuery();
 
             if (rs.next()){
-                String id = rs.getString("id");
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String ic = rs.getString("ic");
-                String password = rs.getString("password");
-
-                DomainBuilder domainBuilder = new DomainBuilder();
-                return domainBuilder.newAccount(id, first_name, last_name, password, username);
-                //password = password, phonenumber = username
-
+                return accountDataHandler(rs);
             }else{
                 return null;
             }
@@ -50,14 +79,30 @@ public class AccountDao implements IAccountDao {
         }catch (Exception e){
             e.printStackTrace();
         }
-
-
-
         return null;
     }
 
     @Override
     public Account findAccountByName(String firstName, String lastName) {
+
+        PreparedStatement st;
+        ResultSet rs;
+        String query = "SELECT * FROM `users` WHERE `first_name` = ? AND `last_name` = ? ";
+        try{
+            st = My_CNX.getConnection().prepareStatement(query);
+            st.setString(1, firstName);
+            st.setString(2, lastName);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+                return accountDataHandler(rs);
+            }else{
+                return null;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -76,4 +121,21 @@ public class AccountDao implements IAccountDao {
 
     }
 
+    private Account accountDataHandler(ResultSet resultSet){
+        try{
+            String id = resultSet.getString("id");
+            String first_name = resultSet.getString("first_name");
+            String last_name = resultSet.getString("last_name");
+            String ic = resultSet.getString("ic");
+            String password = resultSet.getString("password");
+            String username = resultSet.getString("username"); //TODO: Check if this correct.
+
+            DomainBuilder domainBuilder = new DomainBuilder();
+            return domainBuilder.newAccount(id, first_name, last_name, password, username);
+            //password = password, phonenumber = username
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
